d4013e20773f22d1e685fdf047c8121f47608c77
Vansh Chauhan <Vanshchauhan1106@gmail.com>
2025-10-23 19:38:50 +0530

    feat: Update SignIn and SignUp components to use virtual routing

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,7 +6,7 @@ const Login = () => {
     <div className="relative flex items-center justify-center min-h-screen p-4 overflow-hidden bg-background isolate">
       <div className="absolute inset-0 -z-10 h-full w-full animate-aurora [background-image:radial-gradient(125%_125%_at_50%_10%,theme(colors.white)_40%,theme(colors.indigo.100)_100%)] dark:[background-image:radial-gradient(125%_125%_at_50%_10%,theme(colors.slate.950)_40%,theme(colors.indigo.900)_100%)]"></div>
       <div>
-        <SignIn path="/login" routing="path" afterSignInUrl={"/dashboard"} />
+        <SignIn routing="virtual" afterSignInUrl={"/dashboard"} />
       </div>
     </div>
   );
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -6,7 +6,7 @@ const SignUpPage = () => {
     <div className="relative flex items-center justify-center min-h-screen p-4 overflow-hidden bg-background isolate">
       <div className="absolute inset-0 -z-10 h-full w-full animate-aurora [background-image:radial-gradient(125%_125%_at_50%_10%,theme(colors.white)_40%,theme(colors.indigo.100)_100%)] dark:[background-image:radial-gradient(125%_125%_at_50%_10%,theme(colors.slate.950)_40%,theme(colors.indigo.900)_100%)]"></div>
       <div>
-        <SignUp path="/signup" routing="path" afterSignUpUrl={"/dashboard"} />
+        <SignUp  routing="virtual" afterSignUpUrl={"/dashboard"} />
       </div>
     </div>
   );
